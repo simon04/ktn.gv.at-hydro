b881df4423fac9989d69cc4ec83757ed1a7bf81a
Simon Legner <Simon.Legner@gmail.com>
2013-06-06 20:14:38 +0200

    Parse lat/long

--- fetch.py
+++ fetch.py
@@ -1,16 +1,22 @@
 import xml.etree.ElementTree as ET
 from urllib.request import urlopen
+from pprint import pprint
 
 def parseGeoRss(f):
   """
-  >>> parseGeoRss('test.xml')
-  [{'station': 'Mauthen - Gail'}]
+  >>> pprint(parseGeoRss('test.xml'))
+  [{'lat': 46.6686111111111,
+    'long': 13.0002777777778,
+    'station': 'Mauthen - Gail'}]
   """
+  ns = {'geo': 'http://www.w3.org/2003/01/geo/wgs84_pos#'}
   feed = ET.parse(f)
   r = list()
   for item in feed.findall('channel/item'):
     r.append({
-      'station': item.findtext('title').strip()
+      'station': item.findtext('title').strip(),
+      'lat': float(item.findtext('geo:Point/geo:lat', namespaces=ns)),
+      'long': float(item.findtext('geo:Point/geo:long', namespaces=ns)),
     })
   return r
 
