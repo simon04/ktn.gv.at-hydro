1cd68db9d15be993bb552d520934785c89c624dc
Simon Legner <Simon.Legner@gmail.com>
2013-06-06 21:35:37 +0200

    Typo

--- fetch.py
+++ fetch.py
@@ -7,7 +7,7 @@ from datetime import datetime
 """
 A Python 3.3 script to fetch hydrography Open Government Data from
 http://www.kagis.ktn.gv.at/279012_DE-OGD-Kaernten_%28beta%29-Ablussmessstationen
-and store it in a SQLite 3 database.
+and store it in an SQLite 3 database.
 
 Data is licensed CC-BY 3.0 ("Land Kärnten - data.ktn.gv.at").
 This script/file is licensed GNU General Public License 3.
