c5a155bcc2cc6f9b47c27f45f09135d12efd8acd
Simon Legner <Simon.Legner@gmail.com>
2013-06-12 01:21:26 +0200

    Extend web visualization and obtain data from API

--- web/app.js
+++ web/app.js
@@ -1,24 +1,38 @@
 'use strict';
 
 var App = angular.module('myApp', []).
-controller('mainCtrl', ['$scope', function ($scope) {
-  $scope.data = data;
-  $scope.chart = [[[1,2],[3,4],[5,6]]];
+controller('mainCtrl', ['$scope', '$http', function ($scope, $http) {
+  $scope.data = $http.get('http://localhost:8281/stations').then(function(d) {
+    return d.data.stations.map(function(s) {
+      s.plotdata = $scope.getSeriesForChart(s.station, 'w');
+      return s;
+    });
+  });
+  $scope.getSeriesForChart = function(sta, wq) {
+    return $http.get('http://localhost:8281/stations/' + sta).then(function(d) {
+      return d.data.data.map(function(i) {
+        return [new Date(i.dt).getTime(), i[wq]];
+      });
+    });
+  };
 }]);
 
 App.directive('chart', function() {
   return {
-    restrict: 'A',
+    restrict: 'C',
     template: '<div></div>',
     replace: true,
+    scope: {plotData: '='},
     link: function(scope, elem, attrs) {
-      var data = scope[attrs.ngModel];
-      $.plot(elem, data, {
-        xaxis: {show: false, ticks: 4},
-        //yaxis: {show: false},
-        grid: {show: true},
-        colors: ['#f00']
-});
+      scope.$watch('plotData', function(data) {
+        if (!data) return;
+        $.plot(elem, [data], {
+          xaxis: {show: false, ticks: 4},
+          //yaxis: {show: false},
+          grid: {show: true},
+          colors: ['#f00']
+        });
+      });
     }
   };
 });
