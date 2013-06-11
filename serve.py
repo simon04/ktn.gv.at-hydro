5c4a4f6b76f7c0e98ff5e3811c926336767956e9
Simon Legner <Simon.Legner@gmail.com>
2013-06-11 22:58:45 +0200

    Refactoring: use sqlite3 shotcut methods

--- serve.py
+++ serve.py
@@ -11,18 +11,15 @@ def enable_cors():
 
 @route('/stations')
 def stations():
-  c = db.cursor()
-  c.execute('select * from stationdata d1 where d1.dt = (select max(dt) from stationdata d2 where d1.station = d2.station)')
-  r = [dict(zip(row.keys(), row)) for row in c.fetchall()]
-  c.close()
+  q = db.execute('select * from stationdata d1 where d1.dt = (select max(dt) from stationdata d2 where d1.station = d2.station)')
+  r = [dict(zip(row.keys(), row)) for row in q]
   return {'stations': r}
 
 @route('/stations/<sta>')
 def station(sta):
   def meta_data():
-    c = db.cursor()
-    c.execute('select station,river,lat,long from stationdata where station = ? order by dt limit 1', [sta])
-    return [dict(zip(row.keys(), row)) for row in c.fetchall()][0]
+    q = db.execute('select station,river,lat,long from stationdata where station = ? order by dt limit 1', [sta])
+    return [dict(zip(row.keys(), row)) for row in q][0]
   def measurement_data(begin):
     c = db.cursor()
     c.execute('select dt,w,q from stationdata where station = ? and dt > ? order by dt', [sta, begin.isoformat()])
